53456cf8ca78879e78c514e2a2e137e063c9ee45
ov7a <ov7a@yandex.ru>
2017-07-03 23:11:10 +0300

    jmh version bump

--- src/jmh/java/utils/Runclass.java
+++ src/jmh/java/utils/Runclass.java
@@ -3,13 +3,14 @@ package utils;
 
 import collections.simple_fold.JavaStreams;
 import jmh.BenchmarkBase;
+import org.openjdk.jmh.infra.Blackhole;
 
 public class Runclass {
     public static void main(String[] args) {
         BenchmarkBase benchmark = new JavaStreams();
 
         //TODO: change collection size
-        benchmark.prepare(JMHHelper.newBlackhole());
+        benchmark.prepare(new Blackhole("Today's password is swordfish. I understand instantiating Blackholes directly is dangerous."));
 
         Object result = benchmark.benchmark();
         System.out.println(result);
